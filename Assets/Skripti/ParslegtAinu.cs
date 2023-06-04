b8fad31c996565e9bee9d17b03346dd977cc0d25
RozeU <rozeu668@gmail.com>
2023-06-04 19:07:30 +0300

    Izveido jauna ainu kura tika uztaisīta par spēles sākuma ekrānu. Lietotājam ir izvēle spēlēt vai iziet no spēles

---
+++ Assets/Skripti/ParslegtAinu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Importē, lai varētu darboties ar ainām
+using UnityEngine.SceneManagement;
+
+public class ParslegtAinu : MonoBehaviour {
+
+	public void UzSakumu()
+	{
+		SceneManager.LoadScene("Sakums", LoadSceneMode.Single);
+	}
+
+	public void UzMasinasAinu()
+	{
+		SceneManager.LoadScene("PilsetasKarte", LoadSceneMode.Single);
+	}
+
+	public void Apturet()
+	{
+		Application.Quit();
+	}
+
+}
